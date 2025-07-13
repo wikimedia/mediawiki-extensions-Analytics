ee731730e881392d126088fd1d3422f2d20e4734
anterdc99 <xuminruixuan@live.cn>
2025-07-13 14:37:41 +0800

    Analytics: Translate special page aliases into Chinese
    Change-Id: Ib9642b88309929989d09204927c5d9b818d14d26

--- Analytics.alias.php
+++ Analytics.alias.php
@@ -31,3 +31,27 @@ $specialPageAliases['sr-ec'] = [
 $specialPageAliases['sr-el'] = [
 	'Analytics' => [ 'Analitika' ],
 ];
+
+/**
+ * Chinese (中文)
+ * @author anterdc99
+ */
+$specialPageAliases['zh'] = [
+	'Analytics' => [ 'Analytics' ],
+];
+
+/**
+ * Simplified Chinese (中文（简体）)
+ * @author anterdc99
+ */
+$specialPageAliases['zh-hans'] = [
+	'Analytics' => [ '分析' ],
+];
+
+/**
+ * Traditional Chinese (中文（繁體）)
+ * @author anterdc99
+ */
+$specialPageAliases['zh-hant'] = [
+	'Analytics' => [ '分析' ],
+];
